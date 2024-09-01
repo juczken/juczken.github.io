36ec70b44feee5fe497a0a2a30ba652e30b90904
juczken <juczken@mial.ru>
2024-09-01 23:02:49 +0500

    Change text

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Другой текст
+          Опять другой текст
         </p>
       </header>
     </div>
