fcdefc1ea49f3687a93dfd770a3f7b26eba4654c
juczken <juczken@mial.ru>
2024-09-26 21:55:49 +0500

    chore: set css module

--- src/features/Cart/ui/CartButton/CartButton.tsx
+++ src/features/Cart/ui/CartButton/CartButton.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
-import './CartButton.css'
+import cn from 'clsx'
+import style from './CartButton.module.css'
 
 type CartButtonProps = {
     count: number,
@@ -7,14 +8,14 @@ type CartButtonProps = {
 
 const CartButton: FC<CartButtonProps> = ({ count }) => {
     return (
-        <div className="cart_container">
+        <div className={style.cartContainer}>
             {count === 0
-                ? <button className="cart_container cartButton" >В корзину</button>
+                ? <button className={cn(style.cartContainer, style.cartButton)} >В корзину</button>
                 : (
-                    <div className="cartInputContainer">
-                        <button className="cartCounter" >-</button>
-                        <input className="cartInput" type="number" value={count} />
-                        <button className="cartCounter">+</button>
+                    <div className={style.cartInputContainer}>
+                        <button className={style.cartCounter} >-</button>
+                        <input className={style.cartInput} type="number" value={count} />
+                        <button className={style.cartCounter}>+</button>
                     </div>
                 )
             }
@@ -23,12 +24,12 @@ const CartButton: FC<CartButtonProps> = ({ count }) => {
 }
 // const CartButton: FC<CartButtonProps> = ({ count }) => {
 //     return (
-//         <div className="cart_container">
-//             <button className="cart_container cartButton" hidden={count > 0}>В корзину</button>
-//             <div className="cartInputContainer" hidden={count === 0}>
-//                 <button className="cartCounter" hidden={count === 0}>-</button>
-//                 <input className="cartInput" type="number" value={count} hidden={count === 0} />
-//                 <button className="cartCounter" hidden={count === 0}>+</button>
+//         <div className={style.cartContainer}>
+//             <button className={cn(style.cartContainer, style.cartButton)} hidden={count > 0}>В корзину</button>
+//             <div className={style.cartInputContainer} hidden={count === 0}>
+//                 <button className={style.cartCounter} hidden={count === 0}>-</button>
+//                 <input className={style.cartInput} type="number" value={count} hidden={count === 0} />
+//                 <button className={style.cartCounter} hidden={count === 0}>+</button>
 //             </div>
 //         </div>)
 // }
