111fafeb06882d3c68279971afe799104aacc336
juczken <juczken@mial.ru>
2024-09-26 18:24:39 +0500

    build: config webpack
    new mask for module defined classNames

--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,14 @@ module.exports = (_, args) => {
             {
               loader: MiniCssExtractPlugin.loader,
             },
-            'css-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  localIdentName: '[name]_[local]-[hash:base64:5]',
+                },
+              },
+            },
           ],
         },
         {
