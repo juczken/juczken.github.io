f9f8a351d9614a822eb2552e36ee1f0af1295a8b
juczken <juczken@mial.ru>
2024-09-26 21:58:02 +0500

    some...

--- src/shared/ui/Layout.tsx
+++ src/shared/ui/Layout.tsx
@@ -1,19 +1,22 @@
 
 import React from 'react';
-// import './Layout.css'
 import { Header } from './Header';
 import { Modal } from './Modal';
+import { ProductItem } from 'src/entities/Product/ui/ProductItem/ProductItem';
 import { Test } from './Test';
-// import { ButtonIntoCart } from '../Buttons/ButtonIntoCart';
-// import { SimpleButton } from '../Buttons/SimpleButton';
-// import { ItemCard, ItemCardFull, ItemCardInCart } from '../ItemCard';
 
 export const Layout: React.FC = () => {
   return (
     <div className='layout'>
       <Header />
+      <ProductItem name='Fine product' photo="src/shared/ui/images/products/down1.png" price={100.15} desc='ооооо оооооо оооооо оооооо оооооо ооооо оооо ооооооооооооооооооо ооооооооооооооооооо ооооооооооооооооо оо ччччччччччччччччч чччччччччччччччччч ччччччччччччччччч чччччччччччччччч чччччччччччччччч еееееееееееееееее ееееееееееееееееее еееееееееееееее ееееееееееееееее еееееееееее ннннннннннннн ннннннннннннннннн ннннннннннннннннн ннннннннннннн ннннннн ььььььььььььь ьььььььььььььььььььь ьььььььььььььььььььь ььььььььььььььььььь длинный текст' />
+      <div style={{width:150}}>
+      <ProductItem name='Fine product' photo="src/shared/ui/images/products/down1.png" price={100.15} desc='ооооо оооооо оооооо оооооо оооооо ооооо оооо ооооооооооооооооооо ооооооооооооооооооо ооооооооооооооооо оо ччччччччччччччччч чччччччччччччччччч ччччччччччччччччч чччччччччччччччч чччччччччччччччч еееееееееееееееее ееееееееееееееееее еееееееееееееее ееееееееееееееее еееееееееее ннннннннннннн ннннннннннннннннн ннннннннннннннннн ннннннннннннн ннннннн ььььььььььььь ьььььььььььььььььььь ьььььььььььььььььььь ььььььььььььььььььь длинный текст' />
+      </div>
+      {/* <img src="src/shared/ui/images/products/down1.png"/>
+      <img src={require("src/shared/ui/images/products/down1.png")}/> */}
       {/* <ItemCard /> */}
-      <Modal />
+      {/* <Modal children={<Test/>}/> */}
       {/* <ButtonIntoCart count={2} /> */}
       {/* <ButtonIntoCart count={0} /> */}
       {/* <SimpleButton /> */}
