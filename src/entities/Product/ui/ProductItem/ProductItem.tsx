5c48bda664df125bd5f2a58897894d032a7c8139
juczken <juczken@mial.ru>
2024-09-26 21:57:40 +0500

    fix: cut ProductItem desc

--- src/entities/Product/ui/ProductItem/ProductItem.tsx
+++ src/entities/Product/ui/ProductItem/ProductItem.tsx
@@ -1,26 +1,23 @@
-import { FC } from "react"
-import CartButton from "src/features/Cart/ui/CartButton/CartButton";
-// import CartButton from "../../../feature/Cart/ui/CartButton/CartButton";
+import React, { FC, useState } from 'react';
+import CartButton from 'src/features/Cart/ui/CartButton/CartButton';
 
-type ProductItemProps = Pick<Product, "price" | "photo" | "name" | "desc">
+type ProductItemProps = Pick<Product, 'price' | 'photo' | 'name' | 'desc'>;
 
 export const ProductItem: FC<ProductItemProps> = ({ price, photo, name, desc }) => {
-
-    return (
-        <div>
-            <div>
-                <img src={photo} alt={`Product ${name}`}/>
-            </div>
-            <div>
-                <h3>{name}</h3>
-            </div>
-            <div>
-                {price} руб.
-            </div>
-            <div>
-                {desc??<p>{desc}</p>}
-            </div>
-            <div><CartButton count={0}/></div>
-        </div>
-    );
-}
+  return (
+    <div>
+      <div>
+        <img src={photo} alt={`Product ${name}`} />
+        {/* <img src={require(photo)} alt={`Product ${name}`} /> */}
+      </div>
+      <div>
+        <h3>{name}</h3>
+      </div>
+      <div>{price} руб.</div>
+      <div>{desc && <p>{desc.length>100?desc.substring(0,97)+"...":desc}</p>}</div>
+      <div>
+        <CartButton count={0} />
+      </div>
+    </div>
+  );
+};
