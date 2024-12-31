0fe8ae9ec1106aae07f5c5e1285270684b4e6ab7
TerpigorevSA <118421759+TerpigorevSA@users.noreply.github.com>
2024-12-31 18:21:14 +0500

    fix: review

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,11 @@
 import React, { useState } from 'react';
 import cn from 'clsx';
 import style from './App.css';
-import ThemeProvider from '../shared/providers/ThemeProvider/ThemeProvider';
 import Layout from '../shared/ui/Layout/Layout';
 import './localization';
-import { LanguageProvider } from '../shared/providers/LanguageProvider/LanguageProvider';
 import { profileMenuItems, authMenuItems, shopMenuItems, adminMenuItems } from 'src/shared/ui/Layout/menuItems';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import ProductsProvider from '../shared/providers/ProductsProvider/ProductsProvider';
-import AuthProvider from '../shared/providers/AuthProvider/AuthProvider';
+import { Route, Routes } from 'react-router-dom';
 import { WithAuthenticationState } from '../shared/hocs/withAuthenticationState';
-import CartProvider from '../shared/providers/CartProvider/CartProvider';
 
 function App() {
   const [menuItems] = useState([...shopMenuItems, ...profileMenuItems, ...adminMenuItems, ...authMenuItems]);
@@ -54,25 +49,13 @@ function App() {
   };
 
   return (
-    <BrowserRouter>
-      <ThemeProvider>
-        <LanguageProvider>
-          <AuthProvider>
-            <ProductsProvider>
-              <CartProvider>
-                <div className={cn(style.App)}>
-                  <Routes>
-                    <Route path="/" element={<Layout />}>
-                      {generateRoutes(menuItems)}
-                    </Route>
-                  </Routes>
-                </div>
-              </CartProvider>
-            </ProductsProvider>
-          </AuthProvider>
-        </LanguageProvider>
-      </ThemeProvider>
-    </BrowserRouter>
+    <div className={cn(style.App)}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {generateRoutes(menuItems)}
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
